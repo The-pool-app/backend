39ef6674633768d64b0a61dc8dbb9830742e4024
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2023-09-28 00:42:14 +0100

    modularized the application for easy debugging

---
+++ src/database/user.entity.ts
@@ -0,0 +1,19 @@
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+}
