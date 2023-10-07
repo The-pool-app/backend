9ac68509f0a7a24d939c51b0c8499fd3376f2859
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2023-10-07 19:04:31 +0100

    added checks for google signin

--- src/auth/strategy/google.strategy.ts
+++ src/auth/strategy/google.strategy.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private database: DatabaseService,
+  ) {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
@@ -28,6 +32,12 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       picture: photos[0].value,
       accessToken,
     };
+    const existingUser = await this.database.user.findUnique({
+      where: { email: user.email },
+    });
+    if (!existingUser) {
+      return null;
+    }
     done(null, user);
   }
 }
