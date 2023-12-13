bf3d61357de81d0b40b65020e515152ce027518c
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2023-12-13 19:37:20 +0100

    updated config for deployment

--- infra-config/main.tf
+++ infra-config/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    azurerm = {
+    digitalocean = {
       source  = "digitalocean/digitalocean"
       version = ">= 2.10.0"
     }
