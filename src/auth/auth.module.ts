b4c80a71222c5c7b26027362bc82dea2ecd3fdc7
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2023-12-13 20:33:32 +0100

    removed notifications

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,6 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
 import { GoogleStrategy } from './strategy/google.strategy';
-//import { NotificationService } from './notification/notification.service';
-//import { NovuProvider } from './notification/notification.provider';
 
 @Module({
   imports: [JwtModule.register({})],
