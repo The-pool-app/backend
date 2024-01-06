be20036bd5bbfcd43e26ff6e069aa925c2367fcf
Lukmon Abdulsalam <abdulsalamlukmon9@gmail.com>
2024-01-06 23:56:53 +0100

    Remove unnecessary import in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
-import { GoogleStrategy } from './strategy/google.strategy';
+import { GoogleStrategy } from './strategy';
 // import { NotificationModule } from 'src/notification/notification.module';
 
 @Module({
